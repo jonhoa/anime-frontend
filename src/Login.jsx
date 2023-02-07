980fca84ca2349bcc6a34761a152fe1e6101261b
Jonathan <jonitoadriano@gmail.com>
2023-02-07 12:49:22 -0600

    Login works

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,10 +1,24 @@
+import axios from "axios";
+import { useState } from "react";
+
 export function Login() { 
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log("tying to login");
+    const params = new FormData(event.target);
+    axios.post("http://localhost:3000/login", params).then(response =>{
+      console.log(response.data);
+      event.target.reset();
+      console.log("logged in");
+    })
+  };
+  
   return (
     <div>
       <h2>Login</h2>
-      <form>
-        Email<input type="email"></input><br/>
-        Password<input type="password"></input><br/>
+      <form onSubmit ={handleLogin}>
+        Email<input type="email" name="email"></input><br/>
+        Password<input type="password" name="password"></input><br/>
         <button type="submit">Login</button>
         <a href ="">Forgot Password?</a>
       </form>
