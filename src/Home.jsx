e2f269c1a2806763bc2ba7ab1525d309d608e945
Jonathan <jonitoadriano@gmail.com>
2023-02-08 12:47:41 -0600

    carousel and tiles

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,23 +3,44 @@ import { useState } from "react";
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-export function Home() { 
-  const params = useParams();
-  const [titles, setTitles] = useState([]);
-
-  const handleTitles = () => {
-    axios.get("http://localhost:3000/all").then(response =>{
-      console.log(response.data);
-      setTitles(response.data);
-    }
-    );
-  };
-
-  useEffect(handleTitles, []);
-
-
-  console.log(titles.name);
+export function Home(props) { 
+  // How to fetch database records independently without looping
+  // want to create a card but without having to use .map to paste the images
   return (
-    <h1>Home</h1>
+    <div>
+      <div id="carouselExample" class="carousel slide">
+        <div class="carousel-inner">
+      {props.items.map(title => (
+          <div class="carousel-item active">
+            <img src={title.image_url} class="d-block w-100" height ="150" width ="150" alt="..."/>
+          </div>
+      ))}
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+      {props.items.map(title => (
+        <div key = {title.id}>
+          <div class = "container">
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+              <div class="col">
+                <div class="card h-100">
+                  <img src={title.image_url} class="card-img-top" alt="..."/>
+                  <div class="card-body">
+                    <h5 class="card-title">{title.name}</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
